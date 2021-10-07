6442ce02b81a21332797a5b53d09ec8ccda970b1
valera_kudryavtsev <shrhannor@gmail.com>
2021-10-07 14:44:39 +0300

    lab8

--- src/ru/mirea/task8/Gif.java
+++ src/ru/mirea/task8/Gif.java
@@ -2,6 +2,7 @@ package ru.mirea.task8;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class Gif extends JFrame {
     public static void main(String[] args) {
@@ -15,9 +16,39 @@ public class Gif extends JFrame {
         setVisible(true);
     }
 
-    @Override
     public void paint(Graphics g) {
-        Image img = new ImageIcon("/Users/valera_kudryavtsev/Downloads/1R7R.gif").getImage();
-        g.drawImage(img, 50, 50, 300, 300, null);//выводим img
+        for(int i=0; i<3; i++) {
+            Image img = new ImageIcon("/Users/valera_kudryavtsev/Downloads/1200px-FC_Barcelona.svg.png").getImage();
+            g.drawImage(img, 50, 50, 300, 300, null);//выводим img
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            img = new ImageIcon("/Users/valera_kudryavtsev/Downloads/1200px-Manchester_City_FC.svg.png").getImage();
+            g.drawImage(img, 50, 50, 300, 300, null);//выводим img
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            img = new ImageIcon("/Users/valera_kudryavtsev/Downloads/FC_Lokomotiv.png").getImage();
+            g.drawImage(img, 50, 50, 300, 300, null);//выводим img
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            img = new ImageIcon("/Users/valera_kudryavtsev/Downloads/34384984195_eec2b41136_o1.png").getImage();
+            g.drawImage(img, 50, 50, 300, 300, null);//выводим img
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
